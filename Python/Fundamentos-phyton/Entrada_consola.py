4c1a8c9c9fa0f244d75289be60e1e383d359102a
Diana Luna desde Lap <dianabelenluna2@gmail.com>
2024-10-21 19:54:18 -0600

    Se realizó el código de el tema "Entrada de datos por consola"

--- Python/Fundamentos-phyton/Entrada_consola.py
+++ Python/Fundamentos-phyton/Entrada_consola.py
@@ -1,23 +1,28 @@
 '''
-Nombre:
-Fecha:
+Nombre: Diana Belen Luna Hernández
+Fecha: 15 de octubre del 2024
 Descripción:
-Entrada de datos por consola para interacturar con el usuario con valores dinámicos.
+Entrada de datos por consola para interacturar con el usuario con valores dinámicos, los números son leídos como cadenas de texto, por lo que al sumar estos valores sin hacer un casting a un tipo numérico.
 '''
-
-# Comentar sobre la función input.
+# La función input permite al usuario introducir datos por consola.
 numero1_cadena = input("Introduce un número decimal: ") #Lee el primer número
 numero2_cadena = input("Introduce otro número decimal: ") #Lee el sengundo número
+# Aquí se está concatenando (uniendo) las dos cadenas introducidas por el usuario.
+# Al no hacer un casting de tipos de variables, las dos cadenas se tratan como texto.
 resultado_cadena = numero1_cadena + numero2_cadena # Verificar qué es lo que realiza esta instrucción (ver el print).
 print()
 print(" ****  Recibir número sin un casting de varibles  ****")
+# Muestra las dos cadenas concatenadas.
 print(f"El resultado de {numero1_cadena} y {numero2_cadena} es: {resultado_cadena}")
 
-# Comentar por qué se realiza el casting de variables.
+# El "casting" de variables convierte una variable de un tipo a otro.
+# se convierte la cadena introducida por el usuario a un tipo de dato flotante .
 numero1_float = float(numero1_cadena)
 numero2_float = float(numero2_cadena)
+# Aquí, los valores convertidos se suman como números reales, no como texto.
 resultado_float = numero1_float + numero2_float # Verificar qué es lo que realiza de esta manera y compáralo.
 print()
 print(" ****  Casting de varibles  ****")
+# Muestra el resultado de la suma de los dos números decimales.
 print(f"El resultado de {numero1_float} y {numero2_float} es: {resultado_float}")
 
