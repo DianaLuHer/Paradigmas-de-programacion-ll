19a70c5713ec0d596101f2702b114bdf3a471f57
Diana Luna desde Lap <dianabelenluna2@gmail.com>
2024-10-23 10:05:00 -0600

    Se realizó el código de el tema "Operadores aritméticos"

---
+++ Python/Operadores_en_python/Operadores_asignacion_compuestos.py
@@ -0,0 +1,28 @@
+"""
+Diana Belen Luna Hernández
+17/10/2024
+"""
+
+n1,n2=float(input("Ingresa el primer número: ")),float(input("Ingresa el segundo número: "))
+
+n1 += 4
+print(n1)
+n2 -= 6
+print(n2)
+n1 *= 21
+print(n1)
+n2 /= 11
+print(n2)
+n1 %= 14
+print(n1)
+n2 **= 2
+print(n2)
+n2 //= 3
+print(n2)
+
+
+
+
+
+
+
---
+++ Python/Operadores_en_python/Operadores_de_asignacion.py
@@ -0,0 +1,43 @@
+"""
+Diana Belen Luna Hernández
+17/10/2024
+"""
+
+#asignacion multiple
+n1,n2=7,10
+print(n1)
+print(n2)
+
+n3,cad,n4=11,"Hola",9
+print("")
+print(n3)
+print(cad)
+print(n4)
+
+suma,resta=n1+n2,n3-n4
+print("")
+print(suma)
+print(resta)
+
+#Asignación encadenada
+n5=n6=n7=12
+print("")
+print(n5)
+print(n6)
+print(n7)
+
+#intercambio de variables
+print("Intercambio de variables")
+n8=int(input("Ingresa un número entero: "))
+print("")
+n9=int(input("Ingresa un número entero: "))
+
+print(n8,n9)
+
+n9,n8=n8,n9
+
+print(n8,n9)
+
+#Multiples valores de entrada
+print("Multiple valores de entrada")
+n10,n11=int(input("Ingrese el primer número: ")),int(input("Ingrese el segundo número: "))
