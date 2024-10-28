2eb0033c8eeb07fda218ecc795afd348ec5d057f
Diana Luna desde Lap <dianabelenluna2@gmail.com>
2024-10-28 11:06:20 -0600

    Se realizó el código de el tema "Operadores_asignacion", programa que realiza una serie de operaciones matemáticas con dos números ingresados por el usuario.

--- Python/Operadores_en_python/Operadores_asignacion_compuestos.py
+++ Python/Operadores_en_python/Operadores_asignacion_compuestos.py
@@ -1,24 +1,37 @@
-"""
-Diana Belen Luna Hernández
-17/10/2024
-"""
+# Nombre: Diana Belen Luna Hernández
+# Fecha: 17/10/2024
+# Descripción: Este código realiza una serie de operaciones matemáticas con dos números ingresados por el usuario.
 
-n1,n2=float(input("Ingresa el primer número: ")),float(input("Ingresa el segundo número: "))
+# Se solicitan dos números al usuario y se convierten a tipo flotante
+n1, n2 = float(input("Ingresa el primer número: ")), float(input("Ingresa el segundo número: "))
 
+# Se suma 4 al primer número
 n1 += 4
-print(n1)
+print(n1)  # Imprime el nuevo valor de n1
+
+# Se resta 6 al segundo número
 n2 -= 6
-print(n2)
+print(n2)  # Imprime el nuevo valor de n2
+
+# Se multiplica el primer número por 21
 n1 *= 21
-print(n1)
+print(n1)  # Imprime el nuevo valor de n1
+
+# Se divide el segundo número entre 11
 n2 /= 11
-print(n2)
+print(n2)  # Imprime el nuevo valor de n2
+
+# Se calcula el módulo de n1 entre 14 (el residuo de la división de n1 entre 14)
 n1 %= 14
-print(n1)
+print(n1)  # Imprime el nuevo valor de n1
+
+# Se eleva el segundo número al cuadrado
 n2 **= 2
-print(n2)
+print(n2)  # Imprime el nuevo valor de n2
+
+# Se realiza una división entera de n2 entre 3
 n2 //= 3
-print(n2)
+print(n2)  # Imprime el nuevo valor de n2
 
 
 
