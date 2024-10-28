bf749bc837dc60b524892d2f0065371c00f48ee3
Diana Luna desde Lap <dianabelenluna2@gmail.com>
2024-10-28 10:36:13 -0600

    Se realizó el código de el tema "Operadores aritméticos", programa que realiza operaciones matemáticas básicas entre dos números.

--- Python/Operadores_en_python/Operadores_aritmeticos.py
+++ Python/Operadores_en_python/Operadores_aritmeticos.py
@@ -1,25 +1,26 @@
 """
-Diana Belen Luna Hernández
-17/10/2024
+Nombre: Diana Belen Luna Hernández
+Fecha: 17/10/2024
+Descripción: Este programa realiza operaciones matemáticas básicas entre dos números enteros.
 """
-num1 = int(input("Ingresa el primer número entero: "))
-num2 = int(input("Ingresa el segundo número entero: "))
+#Solicita al usuario que ingresen los números, especificando que sean enteros.
+num1,num2 = int(input("Ingresa el primer número entero: ")),int(input("Ingresa el segundo número entero: "))
 
-suma = num1+num2
-resta = num1-num2
-mul= num1*num2
-div= num1/num2 #division flotante
-mod= num1%num2
-exp= num1 ** num2 #exponente
-ddiv= num1 // num2 #divicion entera
+suma = num1+num2 # Realiza la suma entre número1 y número2.
+resta = num1-num2 # Realiza la resta entre número1 y número 2.
+mul= num1*num2 # Realiza la multiplicación entre número1 y número 2.
+div= num1/num2 # Realiza la división flotante entre número1 y número 2.
+mod= num1%num2 # Realiza el módulo (resto de la división) entre num1 y num2
+exp= num1 ** num2 # Calcula el exponente (número1 elevado a la número2)
+ddiv= num1 // num2 # Realiza la división entera entre num1 y num2
 
-print(f"El resultado de la suma de {num1} y {num2} es: {suma}")
-print(f"El resultado de la resta de {num1} y {num2} es: {resta}")
-print(f"El resultado de la multiplicación de {num1} y {num2} es: {mul}")
-print(f"El resultado de la división de {num1} y {num2} es: {div:.3f}")
-print(f"El resultado del módulo {num1} y {num2} es: {mod}")
-print(f"El resultado del esponente de  {num1} y {num2} es: {exp}")
-print(f"El resultado de la división entera de {num1} y {num2} es: {ddiv}")
+print(f"El resultado de la suma de {num1} y {num2} es: {suma}") #Imprime el resultado de la suma.
+print(f"El resultado de la resta de {num1} y {num2} es: {resta}")#Imprime el resultado de la resta.
+print(f"El resultado de la multiplicación de {num1} y {num2} es: {mul}") #Imprime el resultado de la multiplicación.
+print(f"El resultado de la división de {num1} y {num2} es: {div:.3f}")#Imprime el resultado de la división flotante.
+print(f"El resultado del módulo {num1} y {num2} es: {mod}") #Imprime el resultado del módulo.
+print(f"El resultado del esponente de  {num1} y {num2} es: {exp}") #Imprime el resultado del exponente.
+print(f"El resultado de la división entera de {num1} y {num2} es: {ddiv}") #Imprime el resultado de la división entera .
 
 
 
