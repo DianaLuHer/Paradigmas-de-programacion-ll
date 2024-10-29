0ee7156f122c9c109cbce18b53c8971beaaaabbb
Diana Luna desde Lap <dianabelenluna2@gmail.com>
2024-10-28 21:04:26 -0600

    Se realizó el código de el tema "Sentencia_if", programa que verifica si una persona es mayor de edad en función de la edad ingresada.

---
+++ Python/Sentencias-de-control/Sentencia_if.py
@@ -0,0 +1,14 @@
+"""
+Nombre: Diana Belen Luna Hernández
+Fecha: 23/10/2024
+Descripción: Este programa verifica si una persona es mayor de edad en función de la edad ingresada.
+"""
+
+# Solicita al usuario que ingrese su edad y convierte la entrada en un número entero
+edad = int(input("Ingrese su edad: "))
+
+# Verifica si la edad es mayor o igual a 18
+if edad >= 18:
+    # Si la condición se cumple, imprime que la persona es mayor de edad
+    print("Eres mayor de edad")
+print("Codigo de ejemplo")
